006edbf45ffa8fa2a44dc77a10d77cea62b9da68
findthelorax <findthelorax@gmail.com>
2024-01-08 21:08:27 -0500

    login

---
+++ routes/LogoinRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const LoginController = require('../controllers/LoginController');
+
+router.post('/signup', (req, res, next) => {
+    LoginController.signup(req, res, next);
+});
+router.post('/login', (req, res, next) => {
+    LoginController.login(req, res, next);
+});
+router.get('/logout', (req, res, next) => {
+    LoginController.logout(req, res, next);
+});
+
+module.exports = router;
